460bf237e9c9963b736e6c531d26247ff6b1618d
niuyadong <niuyadong@shengqugames.com>
2025-02-11 17:31:48 +0800

    添加国际化支持及登录页面基础结构

---
+++ src/view/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>index</div>
+</template>
+
+<script>
+export default {
+  data() {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped></style>
